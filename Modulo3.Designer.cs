c16de779f3174eba63646e0a96cf6fe5474cfd66
andresJolaya <acjulio@ucundinamarca.edu.co>
2021-09-23 16:48:15 -0500

    modulo 3

--- Modulo3.Designer.cs
+++ Modulo3.Designer.cs
@@ -30,14 +30,14 @@ namespace Parcial
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.fra = new System.Windows.Forms.RichTextBox();
+            this.res = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(338, 389);
+            this.button1.Location = new System.Drawing.Point(303, 378);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -45,39 +45,40 @@ namespace Parcial
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(270, 26);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(244, 96);
-            this.richTextBox1.TabIndex = 5;
-            this.richTextBox1.Text = "";
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Location = new System.Drawing.Point(283, 224);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(231, 96);
-            this.richTextBox2.TabIndex = 6;
-            this.richTextBox2.Text = "";
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(338, 163);
+            this.button2.Location = new System.Drawing.Point(303, 141);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(96, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Pangrama";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fra
+            // 
+            this.fra.Location = new System.Drawing.Point(195, 28);
+            this.fra.Name = "fra";
+            this.fra.Size = new System.Drawing.Size(300, 96);
+            this.fra.TabIndex = 11;
+            this.fra.Text = "";
+            // 
+            // res
+            // 
+            this.res.Location = new System.Drawing.Point(182, 184);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(334, 168);
+            this.res.TabIndex = 12;
+            this.res.Text = "";
             // 
             // Modulo3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(659, 521);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.fra);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Modulo3";
             this.Text = "Modulo 3";
@@ -87,8 +88,8 @@ namespace Parcial
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox fra;
+        private System.Windows.Forms.RichTextBox res;
     }
 }
--- Modulo3.cs
+++ Modulo3.cs
@@ -17,15 +17,40 @@ namespace Parcial
             InitializeComponent();
         }
 
-        private void btn_Click(object sender, EventArgs e)
-        {
-            
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<String> frase = new List<string>();
+            string cadena = " ";
+            int contador = 0;
+            char[] arrcadena;
+            string quit = string.Empty;
+            cadena = fra.Text.Replace(" ","");
+            arrcadena = cadena.ToCharArray();
+            for (int i = 0; i < arrcadena.Length; i++)
+            {
+                for (int j = 0; j < arrcadena.Length; j++)
+                {
+
+                    if (arrcadena[i] == arrcadena[j])
+                    {
+                        contador++;
+                    }
+                }
+                frase.Add(arrcadena[i] + " " + contador + "\n");
+                Console.WriteLine(quit);
+                contador = 0;
+            }
+            HashSet<string> Sinfrase = new HashSet<string>(frase);
+            foreach (String var in Sinfrase)
+            {
+                quit = quit + var.ToString();
+                res.Text = (quit);
+            }
         }
     }
     }
